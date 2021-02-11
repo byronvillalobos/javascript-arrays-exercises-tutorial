b1b547679cb5bde0110af7104815cc0a17f83281
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-11 20:27:00 +0000

    delivered

--- exercises/07.1-Finding-Waldo/app.js
+++ exercises/07.1-Finding-Waldo/app.js
@@ -2,6 +2,8 @@ var people = [ 'Lebron','Aaliyah','Diamond','Dominique','Aliyah','Jazmin','Darne
 
 //loop here to find waldo, use the if conditionals
  for(let i = 0; i<people.length;i++) {
-    console.log(people[i]);
-	if (people[i]='Waldo') break;
+
+    if (people[i]='Waldo') console.log(people[i]);
+break;
+    
 }
