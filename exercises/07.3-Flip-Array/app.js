504d77fb6b2b3c7d7434ab1e7f4599878b85a97c
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-12 03:19:35 +0000

    delivered

--- exercises/07.3-Flip-Array/app.js
+++ exercises/07.3-Flip-Array/app.js
@@ -1,3 +1,12 @@
 var arr = [45,67,87,23,5,32,60];
-
+var newarr = [];
+console.log(arr);
 //Your code here.
+for (var i=arr.length-1; i>-1; i=i-1) {
+newarr.push(arr[i])
+console.log(newarr);
+
+}
+
+
+
