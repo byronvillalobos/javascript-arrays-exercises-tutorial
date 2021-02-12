8b280c7c81822ddc8d8c759ae23cdafd1409d0ab
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-12 03:56:02 +0000

    delivered

--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -1,3 +1,15 @@
 var mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
 
 //your code here
+let newarr = [];
+
+
+for (let i=0; i< mix.length; i++) {
+newarr.push(typeof mix[i])
+
+}
+
+
+console.log(newarr);
+
+ 
