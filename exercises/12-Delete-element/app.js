3e7b1f66bca753c3314a1a7b30d8e131d11eacaf
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-20 01:33:13 +0000

    my profile

--- exercises/12-Delete-element/app.js
+++ exercises/12-Delete-element/app.js
@@ -1,10 +1,18 @@
 var people = ['juan','ana','michelle','daniella','stefany','lucy','barak'];
 
-function deletePerson(personName)
-{
-	//your code here
+  
+function deletePerson(personName) {
+ //your code here
+      let newArray = [];
+      for (let i = 0; i < people.length; i++){
+        if (people[i] != personName){
+          newArray.push(people[i])
+        }
+      }
+      return newArray
 }
 
-console.log(deletePerson('daniella'));
-console.log(deletePerson('juan'));
+
 console.log(deletePerson('emilio'));
+
+
