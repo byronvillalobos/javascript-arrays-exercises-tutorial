3b2faa405bab9f447b5c434f1ff09c546703a890
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-20 02:48:50 +0000

    my profile

--- exercises/15-Max-integer-from-array/app.js
+++ exercises/15-Max-integer-from-array/app.js
@@ -1,1 +1,15 @@
-var myArray = [43,23,6,87,43,1,4,6,3,67,8,3445,3,7,5435,63,346,3,456,734,6,34];
+var people = [43,23,6,87,43,1,4,6,3,67,8,3445,3,7,5435,63,346,3,456,734,6,34];
+var aux= 0
+
+ //your code here
+      for (let i = 0; i < people.length; i++){
+        if (people[i]>aux){
+          aux=people[i]
+        }
+      }
+ 
+
+
+
+console.log(aux);
+
