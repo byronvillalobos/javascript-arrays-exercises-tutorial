548f1b5cbf6ac4cb351c7310eacf31ce3f486905
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-22 01:57:50 +0000

    my profile

--- exercises/20.1-Map-an-array/app.js
+++ exercises/20.1-Map-an-array/app.js
@@ -1,9 +1,11 @@
 var arrayOfCelsiusValues = [-2,34,56,-10];
 
-var arrayOfFahrenheitValues = arrayOfCelsiusValues.map(function(value){
+var arrayOfFahrenheitValues = arrayOfCelsiusValues.map(function(value, index){
 	
 	//add your code here and return the new value
-	
+	    return (value*9/5)+32;
 });
 
 console.log(arrayOfFahrenheitValues);
+
+
